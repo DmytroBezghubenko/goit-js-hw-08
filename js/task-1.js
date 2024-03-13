67fc843fb1da1234402c8b644f48df810c8c6c1a
DmytroBezghubenko <dimon14032@gmail.com>
2024-03-13 21:49:15 +0200

    1

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-const liItem = document.querySelectorAll(".item");
-console.log(`Number of categories: ${liItem.length}`);
-liItem.forEach(element => {
-    console.log("Category:", element.querySelector('h2').textContent);
-    const liElements = element.querySelectorAll('li');
-    console.log("Elements:", liElements.length);
-});
